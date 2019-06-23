36ef3d5a11b4a888f6c83c58049c9ccf0b737511
ryverine <rwcheek@gmail.com>
2019-06-23 15:38:04 -0400

    Index page has link to ALL LOCATIONS. Locations page lists all Locations and the locations link to a specific LOCATION page that lists all stores for that location.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,18 +2,11 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Location.findAll({
-      include: [db.Store]
-    }).then(function(dbLocations) {
-   
+  app.get("/", function(req, res) 
+  {
       res.render("index", {
         msg: "Welcome!"
-        //locations: dbLocations
-        //stores: Stores
       });
-
-    });
   });
 
   // load locations page
@@ -24,27 +17,40 @@ module.exports = function(app) {
       include: [db.Store]
     }).then(function(dbLocations) 
     {
-      console.log("dbLocations", dbLocations);
       res.render("locations", {
         locations: dbLocations
       });
-
     });
   });
 
-
   // load location page
   // page for specific location 
+  app.get("/locations/:id", function(req, res) 
+  {
+    db.Location.findOne(
+    {
+      include: [db.Store], 
+      where: { id: req.params.id }
+    }).then(function(dbLocation) 
+    {
+      console.log("dbLocations.dataValues", dbLocation.dataValues);
+      res.render("location", {
+        location: dbLocation
+      });
+    });
+  });
 
 
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
+
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });
     });
+
   });
 
   // Render 404 page for any unmatched routes
